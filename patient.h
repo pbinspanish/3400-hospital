0bff18ddf398b558f29f4ae63daf40f104940a3b
Hadiyah Arif <93851377+Hadiyahh@users.noreply.github.com>
2025-03-19 17:05:52 -0400

    Update patient.h
    changed a letter

--- patient.h
+++ patient.h
@@ -16,7 +16,7 @@ class Patient {
         time_t P_dischargeDate;
 
 public:
-    Patient(int id, string& fn, string& ln, string& phone, string& disease, string& treatment) : P_id(id), P_fname(fn), P_Lname(ln), P_phoneNumber(phone), P_disease(disease), P_treatment(treatment) {}
+    Patient(int id, string& fn, string& ln, string& phone, string& disease, string& treatment) : P_id(id), P_fname(fn), P_lname(ln), P_phoneNumber(phone), P_disease(disease), P_treatment(treatment) {}
 
     void save(Database& db) const {
         string sql = "INSERT INTO patients (P_id, P_fname, P_lname, P_phoneNumber, P_disease, P_treatment) VALUES ('" +
