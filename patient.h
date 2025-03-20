07f37dea41cbee94fbbf68dfe3295e483ea9714d
faria3islam <156473031+faria3islam@users.noreply.github.com>
2025-03-19 20:23:16 -0400

    Update patient.h

--- patient.h
+++ patient.h
@@ -18,14 +18,7 @@ class Patient {
 public:
     Patient(int id, string& fn, string& ln, string& phone, string& disease, string& treatment) : P_id(id), P_fname(fn), P_lname(ln), P_phoneNumber(phone), P_disease(disease), P_treatment(treatment) {}
 
-    void save(Database& db) const {
-        string sql = "INSERT INTO patients (P_id, P_fname, P_lname, P_phoneNumber, P_disease, P_treatment) VALUES ('" +
-                          P_id + "', '" + P_fname + "', '" + P_lname + "', '" + P_phoneNumber + "', '" + P_disease + "', '" + P_treatment + "');";
-        db.executeSQL(sql);
-    }
+    void save(Database& db) const
 
-    static void getPatient(Database& db, int id) {
-        string sql = "SELECT * FROM patients WHERE P_id = " + to_string(id) + ";";
-        db.executeSQL(sql);
-    }
+    void getPatient(Database& db, int id)
 };
