260c5df3471c862e1c4b6417a7d6f0e770f1c2aa
faria3islam <156473031+faria3islam@users.noreply.github.com>
2025-03-22 19:59:59 -0400

    Update patient.cpp

--- patient.cpp
+++ patient.cpp
@@ -1,13 +1,30 @@
 #include "patient.h"
+#include <iostream>
 
-    Patient(int id, string& fn, string& ln, string& phone, string& disease, string& treatment) : P_id(id), P_fname(fn), P_Lname(ln), P_phoneNumber(phone), P_disease(disease), P_treatment(treatment) {}
+    Patient(int id, string& fn, string& ln, string& phone, string& disease, string& treatment) 
+        : P_id(id), P_fname(fn), P_Lname(ln), P_phoneNumber(phone), P_disease(disease), P_treatment(treatment) {}
 
-    void save(Database& db) const {
+    void Patient::save(Database& db) const {
         string sql = "INSERT INTO patients (P_id, P_fname, P_lname, P_phoneNumber, P_disease, P_treatment) VALUES ('" + to_string(P_id) + "', '" + P_fname + "', '" + P_lname + "', '" + P_phoneNumber + "', '" + P_disease + "', '" + P_treatment + "');";
         db.executeSQL(sql);
     }
 
-    void getPatient(Database& db, int id) {
-        string sql = "SELECT * FROM patients WHERE P_id = " + to_string(id) + ";";
-        db.executeSQL(sql);
+    Patient Patient::getPatient(Database& db, int id) {
+        while (true) {
+            string sql = "SELECT P_id, P_fname, P_lname, P_phoneNumber, P_disease, P_treatment FROM patients WHERE P_id = " + to_string(id) + ";";
+            vector<vector<string>> result = db.executeQuery(sql);
+    
+            if (!result.empty()) {
+                int P_id = stoi(result[0][0]);
+                string P_fname = result[0][1];
+                string P_lname = result[0][2];
+                string P_phoneNumber = result[0][3];
+                string P_disease = result[0][4];
+                string P_treatment = result[0][5];
+    
+                return Patient(P_id, P_fname, P_lname, P_phoneNumber, P_disease, P_treatment);
+            }     
+            cout << "No patient found with ID: " << id << ". Please enter a valid patient ID: ";
+            cin >> id;
+        }
     }
