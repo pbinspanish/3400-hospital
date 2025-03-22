3af25d7b26c28107e695943bde13e0198373109d
faria3islam <156473031+faria3islam@users.noreply.github.com>
2025-03-22 19:47:55 -0400

    Update doctor.h

--- doctor.h
+++ doctor.h
@@ -11,17 +11,17 @@ class Doctor {
         string D_name;
 
 public:
-    Doctor(int id, string& name) : D_id(id), D_name(name) {}
+    Doctor(int id, string& name);
 
     void save(Database& db) const;
 
-    void Doctor::requestOrder(Database& db, const string& prescription);
+    void requestOrder(Database& db, const string& prescription);
     
     void primary(Database& db, Patient& patient) const;
 
-    void Doctor::attend(Database& db, Patient& patient);    
+    void attend(Database& db, Patient& patient);
 
     void discharge(Database& db, Patient& patient) const;
 
-    void doctorMenu(Database& db, Doctor& doctor);
-}
+    void doctorMenu(Database& db);
+};
