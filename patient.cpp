fe4a708214ce784429b455eda635ccad6aa304b3
Hadiyah Arif <93851377+Hadiyahh@users.noreply.github.com>
2025-03-24 15:35:35 -0400

    Update patient.cpp

--- patient.cpp
+++ patient.cpp
@@ -3,7 +3,7 @@
     //Patient::
     Patient(int id, string& fn, string& ln, string& phone, string& disease, string& treatment) 
         : P_id(id), P_fname(fn), P_Lname(ln), P_phoneNumber(phone), P_disease(disease), P_treatment(treatment) {}
-
+                                 //P_lname(ln)?
     void Patient::save(Database& db) const {
         string sql = "INSERT INTO patients (P_id, P_fname, P_lname, P_phoneNumber, P_disease, P_treatment) VALUES ('" + to_string(P_id) + "', '" + P_fname + "', '" + P_lname + "', '" + P_phoneNumber + "', '" + P_disease + "', '" + P_treatment + "');";
         db.executeSQL(sql);
