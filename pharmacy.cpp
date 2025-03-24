8be72e478ae1b1ecd54dea8cefd373bbaf6b4983
faria3islam <156473031+faria3islam@users.noreply.github.com>
2025-03-24 16:16:34 -0400

    Update pharmacy.cpp

--- pharmacy.cpp
+++ pharmacy.cpp
@@ -6,9 +6,15 @@ using namespace std;
 Pharmacy::Pharmacy(const string& name) : name(name) {}
 
 void Pharmacy::shipDrugs() {
+    // added by F: to add to sql database
+    // string sql = "INSERT INTO shipments (pharmacyName, shipmentDate) VALUES ('" + name + "', datetime('now'));";
+    // db.executeSQL(sql);
     cout << "Pharmacy " << name << " is shipping drugs..." << endl;
 }
 
 void Pharmacy::billHospital() {
+    // added by F: to add to sql database
+    // string sql = "INSERT INTO billing (pharmacyName, billAmount, billingDate) VALUES ('" + name + "', " + to_string(amount) + ", datetime('now'));";
+    // db.executeSQL(sql);
     cout << "Billing hospital for drugs from " << name << "..." << endl;
 }
