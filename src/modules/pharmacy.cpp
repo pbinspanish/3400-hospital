5bb55b0d1c33f3650409a2f693af74ff66f9927d
Hadiyah Arif <93851377+Hadiyahh@users.noreply.github.com>
2025-04-01 00:26:48 -0400

    Update pharmacy.cpp

--- src/modules/pharmacy.cpp
+++ src/modules/pharmacy.cpp
@@ -1,20 +1,19 @@
 #include "pharmacy.h"
 #include <iostream>
 
-using namespace std;
 
-Pharmacy::Pharmacy(const string& name) : name(name) {}
+Pharmacy::Pharmacy(const string& pharmacy_name) : pharmacy_name(pharmacy_name) {}
 
-void Pharmacy::shipDrugs() {
-    // added by F: to add to sql database
-    // string sql = "INSERT INTO shipments (pharmacyName, shipmentDate) VALUES ('" + name + "', datetime('now'));";
-    // db.executeSQL(sql);
-    cout << "Pharmacy " << name << " is shipping drugs..." << endl;
+void Pharmacy::shipDrugs(Database& db) {
+    std::string sql = "INSERT INTO shipments (pharmacyName, shipmentDate) VALUES ('" + pharmacy_name + "', datetime('now'));";
+    db.executeSQL(sql);
+    std::cout << "Pharmacy " << pharmacy_name << " is shipping drugs..." << std::endl;
 }
 
-void Pharmacy::billHospital() {
-    // added by F: to add to sql database
-    // string sql = "INSERT INTO billing (pharmacyName, billAmount, billingDate) VALUES ('" + name + "', " + to_string(amount) + ", datetime('now'));";
-    // db.executeSQL(sql);
-    cout << "Billing hospital for drugs from " << name << "..." << endl;
+void Pharmacy::billHospital(Database& db, double amount) {
+    std::string sql = "INSERT INTO billing (pharmacyName, billAmount, billingDate) VALUES ('" + pharmacy_name + "', " + std::to_string(amount) + ", datetime('now'));";
+    db.executeSQL(sql);
+    std::cout << "Billing hospital for drugs from " << pharmacy_name << " ($" << amount << ")" << std::endl;
 }
+
+
