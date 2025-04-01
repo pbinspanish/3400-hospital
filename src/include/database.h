2d9ee5d22900d48a6c4ca92ebb688a1754bf5aa7
Hadiyah Arif <93851377+Hadiyahh@users.noreply.github.com>
2025-04-01 00:32:28 -0400

    Update database.h

--- src/include/database.h
+++ src/include/database.h
@@ -5,12 +5,15 @@
 #include <fstream>
 #include <string>
 #include <SQLiteCpp/SQLiteCpp.h>
+#include <vector>
 
 class Database {
 	public:
 		static void createDatabase();
 		static void insertTestData();
-		static void executeSQL(const std::string& sql);
+		static void executeSQL(const std::string& SQL);
+		std::vector<std::vector<std::string>> executeQuery(const std::string& sql);
+
 };
 
 #endif
