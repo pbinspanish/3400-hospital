d52fa6681d5f023dd130f8e0628f645cc9c40ef2
Hadiyah Arif <93851377+Hadiyahh@users.noreply.github.com>
2025-04-01 00:30:19 -0400

    Update database.cpp

--- src/modules/database.cpp
+++ src/modules/database.cpp
@@ -82,3 +82,25 @@ void Database::executeSQL(const std::string &sql)
 		std::cerr << "SQLite error: " << e.what() << std::endl;
 	}
 }
+
+
+std::vector<std::vector<std::string>> Database::executeQuery(const std::string& sql) {
+    std::vector<std::vector<std::string>> results;
+
+    try {
+        SQLite::Database db("hms.db3", SQLite::OPEN_READWRITE);
+        SQLite::Statement query(db, sql);
+
+        while (query.executeStep()) {
+            std::vector<std::string> row;
+            for (int i = 0; i < query.getColumnCount(); ++i) {
+                row.push_back(query.getColumn(i).getString());
+            }
+            results.push_back(row);
+        }
+    } catch (std::exception& e) {
+        std::cerr << "SQLite error: " << e.what() << std::endl;
+    }
+
+    return results;
+}
